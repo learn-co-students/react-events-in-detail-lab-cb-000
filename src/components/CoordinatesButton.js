eee9cc573e196cdbebc9a850eb3e6d0452f319cd
Felice Forby <forby.1@gmail.com>
2019-03-17 11:25:40 +0900

    set up CoordatesButton and Delayed Button components

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,15 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+  getMouseCoordinates = (event) => {
+    this.props.onReceiveCoordinates([event.clientX, event.clientY]);
+  }
+  
+  render () {
+    return (
+      <button onClick={this.getMouseCoordinates}>Get Mouse Coordinates</button>
+    )
+  }
+}
+
+export default CoordinatesButton;
