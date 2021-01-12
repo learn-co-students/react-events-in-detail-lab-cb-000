d7637116513fd5dce9573323b4184a75e7b8f672
s-blais <stephen@s-blais.com>
2021-01-12 13:44:45 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,13 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+
+  clickHandler = (event) => this.props.onReceiveCoordinates([event.clientX, event.clientY])
+
+  render() {
+    return (
+      <button onClick={this.clickHandler}>This is a button</button>
+    )
+  }
+}
+export default CoordinatesButton
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,21 @@
-// Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component {
+
+  // either persist the event with event.persist() (as shown) or store the event in a variable (as commented), so it can be referenced within the callback of setTimeout(). They both work, but the variable approach doesn't pass the tests.
+  clickHandler = (event) => {
+    // const thisEvent = event
+    event.persist()
+    setTimeout(() => {
+      // this.props.onDelayedClick(thisEvent)
+      this.props.onDelayedClick(event)
+    }, this.props.delay)
+    }
+
+  render() {
+    return (
+      <button onClick={this.clickHandler}>This is a button</button>
+    )
+  }
+}
+export default DelayedButton
